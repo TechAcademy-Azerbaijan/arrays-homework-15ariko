98e7f649396b5eecc81db5e575c3b36362f03f98
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-31 17:19:48 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,19 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let numbers = result.input.split(" ");
+  let max = numbers[0];
+  let max_index = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    let element = parseInt(numbers[i]);
+    if (element >= max) {
+      max = element;
+      max_index = i;
+    }
+  }
+  [numbers[numbers.length - 1], numbers[max_index]] = [
+    max.tostring(),
+    numbers[numbers.length - 1],
+  ];
+  console.log(numbers.join(" "));  
 });
